14b705738d577f46b2f5e61186bcb5c70c83bbf9
mergers-and-acquisitions <mikegalgoczy@gmail.com>
2024-10-14 17:51:37 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# update everything
+sudo apt update
+sudo apt upgrade
+
+sudo apt install libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libxtst6 libxi6 curl python3-pip
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+
+bash Anaconda3-2024.06-1-Linux-x86_64.sh
+
+conda install -c conda-forge jupyterlab
+
+conda install matplotlib numpy pandas
+
+# turn off terminal bell
+# bind 'set bell-style none'
+
+echo "alias python='python3'" >> ~/.bashrc
+echo "export PATH=\"$PATH:/home/user/anaconda3/bin\"" >> ~/.bashrc
+
+echo "#!/bin/bash" > ~/Desktop/dev_env_start.sh
+echo "jupyter lab" >> ~/Desktop/dev_env_start.sh
